8d7b63bce3965f427ba8e11bfe27bac211f61b52
jopalhei <45241927+jopalhei@users.noreply.github.com>
2024-01-09 18:51:23 +0000

    Update CollectAgentLogs.sh

--- CollectAgentLogs.sh
+++ CollectAgentLogs.sh
@@ -1,7 +1,14 @@
+# launch log guest agent collection
 python3 /var/lib/waagent/WALinuxAgent-2.9.1.1/bin/WALinuxAgent-2.9.1.1-py3.8.egg --collect-logs -full
-cd /usr/local/bin
-curl -L https://aka.ms/downloadazcopy-v10-linux
-tar --strip-components=1 --exclude=*.txt -xzvf -
-chmod +x azcopy
+wget https://aka.ms/downloadazcopy-v10-linux
+tar -xvf downloadazcopy-v10-linux
+# Move AzCopy
+sudo rm -f /usr/bin/azcopy
+sudo cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
+sudo chmod 755 /usr/bin/azcopy
+# Clean the kitchen
+rm -f downloadazcopy-v10-linux
+rm -rf ./azcopy_linux_amd64_*/
+# copy resuls to azure storage
 azcopy copy /var/lib/waagent/logcollector/logs.zip "https://logsjopalhei.blob.core.windows.net/exchangefiles?sp=racw&st=2024-01-09T18:22:21Z&se=2024-01-12T02:22:21Z&spr=https&sv=2022-11-02&sr=c&sig=NweWlxwEDI6R%2BRN%2BfURpJQhD3Vvz99SeSN7O5vh0ol4%3D" --from-to LocalBlob
 azcopy copy /var/lib/waagent/logcollector/results.txt "https://logsjopalhei.blob.core.windows.net/exchangefiles?sp=racw&st=2024-01-09T18:22:21Z&se=2024-01-12T02:22:21Z&spr=https&sv=2022-11-02&sr=c&sig=NweWlxwEDI6R%2BRN%2BfURpJQhD3Vvz99SeSN7O5vh0ol4%3D" --from-to LocalBlob
