a2a5e9c1094503176150e3846827985f04fa0f0d
jopalhei <45241927+jopalhei@users.noreply.github.com>
2024-01-09 20:40:42 +0000

    Update CollectAgentLogs.sh

--- CollectAgentLogs.sh
+++ CollectAgentLogs.sh
@@ -1,10 +1,10 @@
 # launch log guest agent collection
 python3 /var/lib/waagent/WALinuxAgent-2.9.1.1/bin/WALinuxAgent-2.9.1.1-py3.8.egg --collect-logs -full
-wget https://aka.ms/downloadazcopy-v10-linux #-O /tmp/downloadazcopy-v10-linux
-#cd /tmp/
-tar -xvf downloadazcopy-v10-linux
+wget https://aka.ms/downloadazcopy-v10-linux -O /tmp/downloadazcopy-v10-linux
+cd /tmp/
+tar -xvf /tmp/downloadazcopy-v10-linux
 # Move AzCopy
-sudo cp ./azcopy_linux_amd64_*/azcopy
+sudo cp ./azcopy_linux_amd64_*/azcopy /tmp
 #sudo chmod 755 /usr/bin/azcopy
 # Clean the kitchen
 #rm -f downloadazcopy-v10-linux
